cfb873a9f5af6504dedc4bed0fa7ba85f48fbdb1
NamPE286 <nambuihung654@gmail.com>
2025-09-26 23:03:15 +0700

    setup JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.arkanoid {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.arkanoid to javafx.fxml;
+    exports org.arkanoid;
+}
