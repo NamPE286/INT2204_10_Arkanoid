3229e520c285c14c89c4ae9e0fd636098b926709
NamPE286 <nambuihung654@gmail.com>
2025-10-20 03:26:50 +0700

    fix: SoundManager

--- src/main/java/org/arkanoid/managers/SoundManager.java
+++ src/main/java/org/arkanoid/managers/SoundManager.java
@@ -7,17 +7,17 @@ import java.util.HashMap;
 /**
  * Manages loading and playing sound effects in the game.
  * <p>
- * This class uses a cache (HashMap) to store loaded sounds and avoid
- * reloading the same sound file multiple times. Sounds are played
- * using FXGL's built-in audio system.
+ * This class uses a cache (HashMap) to store loaded sounds and avoid reloading the same sound file
+ * multiple times. Sounds are played using FXGL's built-in audio system.
  * </p>
  */
 public class SoundManager {
+
     private final static HashMap<String, Sound> soundHashMap = new HashMap<>();
 
     /**
-     * Loads a sound file into the cache.
-     * If the sound already exists in the cache, the method does nothing.
+     * Loads a sound file into the cache. If the sound already exists in the cache, the method does
+     * nothing.
      *
      * @param path The path to the sound file to be loaded.
      */
@@ -33,8 +33,8 @@ public class SoundManager {
     /**
      * Plays a sound from the specified file path.
      * <p>
-     * If the sound has not been loaded yet, it will be loaded first and then played.
-     * If loading fails, a warning message will be printed to the console.
+     * If the sound has not been loaded yet, it will be loaded first and then played. If loading
+     * fails, a warning message will be printed to the console.
      * </p>
      *
      * @param path The path to the sound file to be played.
@@ -52,12 +52,5 @@ public class SoundManager {
         } else {
             System.out.println("Cannot play sound: " + path);
         }
-public class SoundManager {
-    public void load(String path) {
-
-    }
-
-    public void play(String path) {
-
     }
 }
