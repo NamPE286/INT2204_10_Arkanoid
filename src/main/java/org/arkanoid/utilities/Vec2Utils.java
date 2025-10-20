22f91ceb89185ca88896880462a10e0fbf493a68
Long <lelong28032006@icloud.com>
2025-10-20 22:50:07 +0700

    docs: add comment for Vec2Utils class

--- src/main/java/org/arkanoid/utilities/Vec2Utils.java
+++ src/main/java/org/arkanoid/utilities/Vec2Utils.java
@@ -4,10 +4,31 @@ import com.almasb.fxgl.core.math.Vec2;
 import org.arkanoid.entity.GameObject;
 
 public class Vec2Utils {
+    /**
+     * Creates a new 2D vector with the given X and Y components.
+     *
+     * @param x the X component of the vector
+     * @param y the Y component of the vector
+     * @return a new {@link Vec2} instance with the specified components
+     */
     public static Vec2 getVec2(float x, float y) {
         return new Vec2(x, y);
     }
 
+    /**
+     * Calculates the reflected (flipped) velocity vector of a moving object after
+     * colliding with another game object.
+     * <p>
+     * The method determines the side of collision (left, right, top, or bottom)
+     * by comparing the overlap distances between the two objects, then flips
+     * the corresponding velocity component.
+     * </p>
+     *
+     * @param velocity the current velocity vector of the moving object
+     * @param o        the main game object whose velocity is being flipped
+     * @param other    the other game object that {@code o} collided with
+     * @return a new {@link Vec2} representing the flipped velocity after collision
+     */
     public static Vec2 flip(Vec2 velocity, GameObject o, GameObject other) {
         float vx = velocity.x;
         float vy = velocity.y;
@@ -43,6 +64,16 @@ public class Vec2Utils {
         return new Vec2(vx, vy);
     }
 
+    /**
+     * Converts a 2D vector into its corresponding angle in degrees.
+     * <p>
+     * The angle is measured counterclockwise from the positive X-axis,
+     * based on the vector’s direction.
+     * </p>
+     *
+     * @param v the input vector
+     * @return the angle in degrees, in the range [-180°, 180°]
+     */
     public static double getAngle(Vec2 v) {
         double radianAngle = Math.atan2(v.y, v.x);
 
