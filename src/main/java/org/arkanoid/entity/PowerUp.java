0269111a616e5a49c32efb509958395b35afd8be
Nguyen Hunge <nguyenshark379@gmail.com>
2025-10-22 19:10:30 +0700

    feat: Begin to initialize powerpus

---
+++ src/main/java/org/arkanoid/entity/PowerUp.java
@@ -0,0 +1,12 @@
+package org.arkanoid.entity;
+
+public abstract class PowerUp {
+    public static int movedownsp = 5;
+
+    public void loadPU() {
+
+    }
+
+
+    public abstract void Type();
+}
