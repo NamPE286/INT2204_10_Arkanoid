5bec7dc897c82f92c3c0fce0b5977f6d0924b199
NamPE286 <nambuihung654@gmail.com>
2025-10-26 05:37:45 +0700

    feat: Level class

---
+++ src/main/java/org/arkanoid/core/Level.java
@@ -0,0 +1,62 @@
+package org.arkanoid.core;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.arkanoid.entity.Ball;
+import org.arkanoid.entity.Brick;
+import org.arkanoid.entity.NormalBrick;
+import org.arkanoid.entity.Paddle;
+import org.arkanoid.entity.Wall;
+import org.arkanoid.manager.SoundManager;
+
+public class Level {
+
+    private static final int HEIGHT = 768;
+    private static final int WIDTH = 672;
+    private static final int THICK = WIDTH / 28;
+
+    Paddle paddle;
+    Ball ball;
+    List<Brick> bricks = new ArrayList<>();
+
+    private void loadBrickConfiguration() {
+        bricks.add(new NormalBrick(300, 300, 1, 0)
+            .setPaddle(paddle));
+        bricks.add(new NormalBrick(360, 360, 2, 0)
+            .setPaddle(paddle));
+    }
+
+    public void onUpdate(double deltaTime) {
+        paddle.onUpdate(deltaTime);
+        ball.onUpdate(deltaTime);
+
+        for (var brick : bricks) {
+            brick.onUpdate(deltaTime);
+        }
+    }
+
+    public Level() {
+        var leftwall = new Wall(0, 0, HEIGHT, THICK);
+        var topwall = new Wall(0, 48, THICK, WIDTH);
+        var rightwall = new Wall(WIDTH - THICK, 0, HEIGHT, THICK);
+
+        paddle = (Paddle) new Paddle(WIDTH / 2, HEIGHT - 50)
+            .listenToCollisionWith(leftwall)
+            .listenToCollisionWith(rightwall);
+
+        loadBrickConfiguration();
+
+        ball = (Ball) new Ball(WIDTH / 2, HEIGHT - 50 - 100)
+            .setLinearVelocity(300f, 300f)
+            .listenToCollisionWith(paddle)
+            .listenToCollisionWith(leftwall)
+            .listenToCollisionWith(topwall)
+            .listenToCollisionWith(rightwall);
+
+        for (var brick : bricks) {
+            ball.listenToCollisionWith(brick);
+        }
+
+        SoundManager.play("round_start.mp3");
+    }
+}
