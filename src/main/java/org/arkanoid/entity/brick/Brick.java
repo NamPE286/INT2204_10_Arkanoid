6b8b0f159f280268f937fb4b83b8f6313b50b163
Long <lelong28032006@icloud.com>
2025-10-31 12:11:39 +0700

    feat: add powerUpFactory to random spawn power up with different type

--- src/main/java/org/arkanoid/entity/brick/Brick.java
+++ src/main/java/org/arkanoid/entity/brick/Brick.java
@@ -139,6 +139,7 @@ import org.arkanoid.entity.Paddle;
 import org.arkanoid.entity.powerup.LaserPowerUp;
 import org.arkanoid.entity.powerup.PowerUp;
 import javafx.geometry.Point2D;
+import org.arkanoid.factory.PowerUpFactory;
 import org.arkanoid.game.Game;
 
 public abstract class Brick extends GameObject {
@@ -208,8 +209,7 @@ public abstract class Brick extends GameObject {
             SpawnData spawnData = new SpawnData(brickPosition);
 
             if (paddle != null) {
-                powerUp = new LaserPowerUp(spawnData);
-                powerUp.listenToCollisionWith(paddle);
+                powerUp = PowerUpFactory.spawnRandomPowerUp(spawnData, paddle);
             }
 
         }
