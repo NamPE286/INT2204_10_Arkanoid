bdb8dfd117549a47891b6701479e1e405b9daad0
Nguyen Hunge <nguyenshark379@gmail.com>
2025-11-01 22:58:06 +0700

    feat: Add the Disrupt power up

--- src/main/java/org/arkanoid/entity/powerup/DisruptPowerup.java
+++ src/main/java/org/arkanoid/entity/powerup/DisruptPowerup.java
@@ -3,17 +3,18 @@ package org.arkanoid.entity.powerup;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.SpawnData;
-import com.almasb.fxgl.physics.box2d.dynamics.joints.LimitState;
-import javafx.util.Duration;
-import org.arkanoid.component.ExtendComponent;
 import org.arkanoid.entity.Ball;
 import org.arkanoid.entity.EntityType;
 import org.arkanoid.entity.core.GameObject;
 import org.arkanoid.entity.Paddle;
+import org.arkanoid.game.Game;
+import org.arkanoid.level.Level;
 import org.arkanoid.manager.PowerupType;
-import org.arkanoid.manager.SoundManager;
+
 
 import javafx.geometry.Point2D;
+
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.List;
 
@@ -29,14 +30,12 @@ public class DisruptPowerup extends PowerUp {
     }
 
     @Override
-    public void applyEffect(Entity Ballentity) {
-        System.out.println("Create 2 new balls");
+    public void applyEffect(Entity entity) {
+        System.out.println("Need overload function");
+    }
 
-        Ball ball1 = new Ball((int) Ballentity.getX(), (int) Ballentity.getY());
-        Ball ball2 = new Ball((int) Ballentity.getX(), (int) Ballentity.getY());
-
-        ball1.setLinearVelocity(-200, -300);
-        ball2.setLinearVelocity(200, -300);
+    public void applyEffect(Ball curBall) {
+        curBall.createTwins();
     }
 
     @Override
@@ -46,15 +45,11 @@ public class DisruptPowerup extends PowerUp {
             return;
         }
 
-        List<Entity> ballList = FXGL.getGameWorld().getEntitiesByType(EntityType.BALL);
-        Entity mainBall = null;
-        Point2D spawnPos;
-        if (!ballList.isEmpty()) {
-            mainBall = ballList.get(0);
-            spawnPos = mainBall.getCenter();
-            applyEffect(mainBall);
-        } else {
-            System.out.println("Cannot find the main Ball");
+        Level curlevel = Game.getInstance().getCurrentLevel();
+        List<Ball> Balllist = new ArrayList<>(curlevel.getBallTwinslist());
+        Balllist.add(curlevel.getMainBall());
+        for (var b : Balllist) {
+            applyEffect(b);
         }
 
         this.getEntity().removeFromWorld();
