132e9b8d86c29bd377d9723115551e1d8e45e0b2
Nguyen Hunge <nguyenshark379@gmail.com>
2025-11-02 21:31:40 +0700

    fix: Handle the error of twin ball

--- src/main/java/org/arkanoid/entity/powerup/DisruptPowerup.java
+++ src/main/java/org/arkanoid/entity/powerup/DisruptPowerup.java
@@ -1,21 +1,15 @@
 package org.arkanoid.entity.powerup;
 
-import com.almasb.fxgl.dsl.FXGL;
+
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.SpawnData;
 import org.arkanoid.entity.Ball;
-import org.arkanoid.entity.EntityType;
 import org.arkanoid.entity.core.GameObject;
 import org.arkanoid.entity.Paddle;
 import org.arkanoid.game.Game;
 import org.arkanoid.level.Level;
 import org.arkanoid.manager.PowerupType;
-
-
-import javafx.geometry.Point2D;
-
 import java.util.ArrayList;
-import java.util.Optional;
 import java.util.List;
 
 public class DisruptPowerup extends PowerUp {
@@ -45,17 +39,18 @@ public class DisruptPowerup extends PowerUp {
     @Override
     public void onCollisionWith(GameObject e) {
         System.out.println("Disrupt power up collied with paddle");
-        if (!(e instanceof Paddle curpaddle)) {
+        if (!(e instanceof Paddle)) {
             return;
         }
 
         // Maximum number of balls is 24.
         final int maxBallwhenspawn = 9;
         Level curlevel = Game.getInstance().getCurrentLevel();
+
         List<Ball> Balllist = new ArrayList<>(curlevel.getBallTwinslist());
         Balllist.add(curlevel.getMainBall());
-        int currentNumbal = Balllist.size();
 
+        int currentNumbal = Balllist.size();
         int freeslot = maxBallwhenspawn - currentNumbal;
 
         if (freeslot <= 0) {
