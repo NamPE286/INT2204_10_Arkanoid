3b63750bf9019578e4df873cfbc30aaf4a79af94
Nguyen Hunge <nguyenshark379@gmail.com>
2025-11-02 08:52:07 +0700

    feat: Explanation of modify in Movable object

--- src/main/java/org/arkanoid/entity/core/MovableObject.java
+++ src/main/java/org/arkanoid/entity/core/MovableObject.java
@@ -76,17 +76,19 @@ public abstract class MovableObject extends GameObject {
 
             boolean hasCollided = false;
             for (var other : collisionListeners) {
+                // kiểm tra xem vật thể sắp va chạm có tồn tại hay không
                 if (other == null || other.getEntity() == null || !other.getEntity().isActive()) {
                     continue;
                 }
 
 
+                // kiểm tra BBox của các vật thể khác.
                 var otherBBox = other.getEntity().getBoundingBoxComponent();
                 if (otherBBox == null) {
-                    continue; // Bỏ qua nếu gạch không có BBox
+                    continue;
                 }
 
-
+                // kiểm tra Bbox của vật thể này.
                 var thisBBox = getEntity().getBoundingBoxComponent();
                 if (thisBBox == null) {
                     hasCollided = true;
